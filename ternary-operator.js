3b0dc603709ea129e048aead82749554765a0158
hanyongjun <hanyongjun.dev@gmail.com>
2022-09-07 00:30:55 +0900

    삼항연산자

--- ternary-operator.js
+++ ternary-operator.js
@@ -6,4 +6,4 @@ let result = (operandOne.length === operandTwo) ? "hello" : "bye";
 console.log(result);
 
 //조건문 ? --> 조건문이 참일경우 콜론 앞의 값 거짓일경우 콜론 뒤의 값
-//condition ? exprIfTrue : exprIfFalse
+//condition ? exprIfTrue : exprIfFalse 
